a58896c674ba605247b711495640d42471cdf4be
yakumo17s <wo409697458@gmail.com>
2017-08-22 13:40:32 +0800

    init

--- web/settings.py
+++ web/settings.py
@@ -15,7 +15,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '*#g_w$ef72c0$6jnsmmj%^*i9cub)2eb^z6i!9$l2!t942kv@m'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'web.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
@@ -84,11 +81,18 @@ DATABASES = {
         'PORT': 3306
     }
     # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'plc_web',
+    #     'USER': 'root',
+    #     'PASSWORD': 'touhou',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': 3306
+    # }
+    # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -103,8 +107,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
