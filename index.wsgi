60dff61f5dad641d5a384507a56481639976a585
yakumo17s <wo409697458@gmail.com>
2017-08-22 11:37:16 +0800

    init

--- index.wsgi
+++ index.wsgi
@@ -2,5 +2,5 @@ import sae
 import os, sys
 root = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(root, 'site-packages'))
-from plc_web import wsgi
+from web import wsgi
 application = sae.create_wsgi_app(wsgi.application)
--- web/settings.py
+++ web/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'app_yakumo',
+        'USER': '1m4zwn31kn',
+        'PASSWORD': 'i155mii155xx51xkyi0ximxl3hzy3hzk244lk4j0',
+        'HOST': 'w.rdc.sae.sina.com.cn',
+        'PORT': 3306
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
